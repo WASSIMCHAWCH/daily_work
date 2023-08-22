8e4e89f410a112dbe5c4264f97067e625d3f3ba7
wassim chawch <chawchwassim123@gmail.com>
2023-08-22 15:42:01 +0100

    merkle tree pre-reveal Nft

---
+++ merkle tree server/server.js
@@ -0,0 +1,54 @@
+const express = require("express");
+const fs = require("fs");
+const { MerkleTree } = require("merkletreejs");
+const { ethers } = require("ethers");
+
+const app = express();
+
+app.use(express.json());
+
+/*
+
+const encoded = utils.solidityPack(["string", "uint"], [str, num]);
+
+// hash them with keccak256
+const hashed = utils.keccak256(encoded);
+
+
+
+*/
+
+const data = JSON.parse(fs.readFileSync("uri's.json"));
+console.log(data);
+
+const leaves = data.map((x) =>
+  ethers.utils.keccak256(
+    ethers.utils.defaultAbiCoder.encode(["string", "uint"], [x.uri, x.id])
+  )
+);
+
+const tree = new MerkleTree(leaves, ethers.utils.keccak256, {
+  sortPairs: true,
+});
+
+const root = tree.getHexRoot().toString("hex");
+
+app.get("/root", (req, res) => {
+  res.send(root);
+});
+
+app.get("/proof", (req, res) => {
+  const { uri, id } = req.body;
+
+  const leaf = ethers.utils.keccak256(
+    ethers.utils.defaultAbiCoder.encode(["string", "uint"], [uri, id])
+  );
+
+  const proof = tree.getHexProof(leaf);
+
+  res.send(proof);
+});
+
+app.listen(3000, () => {
+  console.log("Server is running on port 3000");
+});
